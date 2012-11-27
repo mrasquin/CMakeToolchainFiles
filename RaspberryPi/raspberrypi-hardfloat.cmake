d2108de6faddbe6e3d7ac946863d9efee198c404
Ben Matthews <ben@kc2vjw.com>
2012-11-27 00:37:23 -0500

    Very basic toolchain file for Raspberry Pi (cross) (hardware float)

---
+++ RaspberryPi/raspberrypi-hardfloat.cmake
@@ -0,0 +1,3 @@
+find_program(CMAKE_C_COMPILER armv6j-hardfloat-linux-gnueabi-gcc)
+find_program(CMAKE_CXX_COMPILER armv6j-hardfloat-linux-gnueabi-g++)
+find_program(CMAKE_Fortran_COMPILER armv6j-hardfloat-linux-gnueabi-gfortran)
